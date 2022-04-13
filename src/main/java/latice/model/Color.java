69074fbe6e41806d4525722077267ad2bbeff038
Théo Mestries <theo.203@hotmail.com>
2022-04-13 15:18:16 +0200

    Add Color

---
+++ src/main/java/latice/model/Color.java
@@ -0,0 +1,10 @@
+package latice.model;
+
+public enum Color {
+	RED,
+	GREEN,
+	GREY,
+	YELLOW,
+	PINK,
+	BLUE,
+}
--- src/main/java/latice/model/Tile.java
+++ src/main/java/latice/model/Tile.java
@@ -2,9 +2,11 @@ package latice.model;
 
 public class Tile {
 	private Shape shape;
+	private Color color;
 
-	public Tile(Shape shape) {
+	public Tile(Shape shape, Color color) {
 		this.shape = shape;
+		this.color = color;
 	}
 
 	
