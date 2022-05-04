4686be24e388466b3f13ba35e8796f5107933e4f
paul rezzonico <paul.rezzo@gmail.com>
2022-05-04 16:11:37 +0200

    ajout d'une liste des couleurs pour la boucle for afin d'instancier les pièces, refractoring en deux méthodes

--- src/main/java/latice/application/Game.java
+++ src/main/java/latice/application/Game.java
@@ -1,9 +1,24 @@
 package latice.application;
 
+import java.util.ArrayList;
+
+import latice.model.Color;
+
 public class Game {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ArrayList<Color> colorList = new ArrayList<>();
+		colorList.add(Color.RED);
+		colorList.add(Color.BLUE);
+		colorList.add(Color.GREEN);
+		colorList.add( Color.GREY);
+		colorList.add(Color.PINK);
+		colorList.add(Color.YELLOW);
+		
+		for (Color color : colorList) {
+			//TODO rajouter les instanciations
+		}
 
 	}
 
