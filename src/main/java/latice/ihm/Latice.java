ef6aa1f7fe9969f79b028cb997da50958b98c5d0
paul rezzonico <paul.rezzo@gmail.com>
2022-05-25 13:26:37 +0200

    refractor the play conroller

--- src/main/java/latice/ihm/Latice.java
+++ src/main/java/latice/ihm/Latice.java
@@ -5,20 +5,18 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import latice.ihm.controller.ExitController;
-import latice.ihm.controller.ParametersController;
 import latice.ihm.controller.PlayController;
 import latice.ihm.view.MenuFX;
-import latice.model.Player;
 
-public class LaticeApp extends Application {
+public class Latice extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		MenuFX menu = new MenuFX(primaryStage);
 		Scene scene = new Scene(menu);
 		menu.getBtnExit().setOnMouseClicked(new ExitController());
-		menu.getBtnGame().setOnMouseClicked(new PlayController(menu, null));
-		menu.getBtnParameters().setOnMouseClicked(new ParametersController(menu));
+		menu.getBtnGame().setOnMouseClicked(new PlayController(menu));
+		
 		primaryStage.setScene(scene);
 		primaryStage.setFullScreen(true);
 		primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/Logo.png")));
--- src/main/java/latice/ihm/controller/PlayController.java
+++ src/main/java/latice/ihm/controller/PlayController.java
@@ -4,6 +4,7 @@ import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.media.AudioClip;
 import javafx.stage.Stage;
+import latice.application.Game;
 import latice.ihm.view.MenuFX;
 import latice.ihm.view.PlayFX;
 import latice.model.Player;
@@ -13,18 +14,19 @@ public class PlayController implements EventHandler<MouseEvent>{
 	private Stage primaryStage;
 	private MenuFX menu;
 	private AudioClip song;
-	private Player player;
+	private Game game;
 	
-	public PlayController(MenuFX menu, Player player) {
+	public PlayController(MenuFX menu) {
 		this.menu = menu;
 		this.song = new AudioClip(getClass().getResource("/song/Loading.mp3").toExternalForm());
+		this.game = new Game();
 	}
 	
 	@Override
 	public void handle(MouseEvent event) {
 		this.menu.getSong().stop();
 		this.song.play();
-		this.primaryStage.getScene().setRoot(new PlayFX(this.player));
+		this.primaryStage.getScene().setRoot(new PlayFX(null));
 		
 	}
 }
