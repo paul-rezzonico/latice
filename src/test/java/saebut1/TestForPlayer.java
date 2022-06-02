b904efe3149e041b12fd443ef3cc79e122943f55
paul rezzonico <paul.rezzo@gmail.com>
2022-06-02 12:19:35 +0200

    add test for player class

--- src/test/java/saebut1/TestForPlayer.java
+++ src/test/java/saebut1/TestForPlayer.java
@@ -11,50 +11,97 @@ import latice.model.Player;
 import latice.model.Symbol;
 import latice.model.Tile;
 
-public class TestForPlayer {
+class TestForPlayer {
 
 	static int RACK_FULL = 5;
 
 	@Test
-	void shouldNotFillRackWhenRackIsFull() {
+	void shouldHaveASizeOfZeroIfTheRackIsEmpty() {
 
-		ArrayList<Tile> stack = new ArrayList<>();
-		stack.add(new Tile(Symbol.AKENO, Color.RED));
-		stack.add(new Tile(Symbol.AKENO, Color.GREY));
-		stack.add(new Tile(Symbol.RIAS, Color.YELLOW));
-		stack.add(new Tile(Symbol.XENOVIA, Color.BLUE));
-		stack.add(new Tile(Symbol.ASIA, Color.GREEN));
-		ArrayList<Tile> rack = rackFullCreation();
+		ArrayList<Tile> stack = createASTackWithFiveTile();
 		Player player = new Player("Player", stack, 0);
 
-		player.fillRack();
+		assertThat(player.sizeRack()).isZero();
 
-		assertThat(player.sizeRack()).isEqualTo(RACK_FULL);
-		assertThat(player.getRack()).usingRecursiveComparison().isEqualTo(rack);
+	}
+
+	@Test
+	void shouldNotHaveASizeOfZeroIfTheRackIsNotEmpty() {
+
+		ArrayList<Tile> stack = createASTackWithFiveTile();
+		Player player = new Player("Player", stack, 0);
+
+		fillRackWithOnlyThreeTiles(player);
+
+		assertThat(player.sizeRack()).isNotZero();
+
+	}
+
+	@Test
+	void shouldHaveASizeOfThree() {
+
+		ArrayList<Tile> stack = createASTackWithFiveTile();
+		Player player = new Player("Player", stack, 0);
+
+		fillRackWithOnlyThreeTiles(player);
+
+		assertThat(player.sizeRack()).isEqualTo(3);
 
 	}
 
 	@Test
 	void shouldFillRackWhenRackIsEmpty() {
 
-		ArrayList<Tile> stack = new ArrayList<>();
-		stack.add(new Tile(Symbol.RIAS, Color.RED));
-		stack.add(new Tile(Symbol.AKENO, Color.BLUE));
-		stack.add(new Tile(Symbol.KONEKO, Color.YELLOW));
-		ArrayList<Tile> rack = rackCreation();
+		ArrayList<Tile> stack = createASTackWithFiveTile();
 		Player player = new Player("Player", stack, 0);
+		Player player2 = new Player("Player", stack, 0);
+		int startSize = player.sizeRack();
 
 		player.fillRack();
 
+		assertThat(player.sizeRack()).isEqualTo(RACK_FULL)
+										.isNotZero()
+										.isNotEqualTo(startSize);
+		assertThat(player.getRack()).usingRecursiveComparison().isNotEqualTo(player2.getRack());
+
 	}
 
 	@Test
-	void shouldFillRackWhenThereAreThreeTilesOnTheRtack() {
+	void shouldNotFillRackWhenRackIsFull() {
+
+		ArrayList<Tile> stack = createASTackWithFiveTile();
+		Player player = new Player("Player", stack, 0);
+		ArrayList<Tile> rack = player.getRack();
+		int startSize = player.sizeRack();
+
+		player.fillRack();
+
+		assertThat(player.sizeRack()).isEqualTo(RACK_FULL);
+		assertThat(player.sizeRack()).isGreaterThan(startSize);
+		assertThat(player.getRack()).usingRecursiveComparison().isEqualTo(rack);
 
 	}
 
 	@Test
-	void shouldFillRackWhenThereStayOnly3TilesOnTheStack() {
+	void shouldFillRackWhenThereAreThreeTilesOnTheRack() {
+
+		ArrayList<Tile> stack = createASTackWithFiveTile();
+		Player player = new Player("Player", stack, 0);
+		fillRackWithOnlyThreeTiles(player);
+		ArrayList<Tile> rack = player.getRack();
+
+		int startSize = player.sizeRack();
+
+		player.fillRack();
+
+		assertThat(player.sizeRack()).isEqualTo(RACK_FULL);
+		assertThat(player.sizeRack()).isGreaterThan(startSize);
+		assertThat(player.getRack()).usingRecursiveComparison().isEqualTo(rack);
+
+	}
+
+	@Test
+	void shouldFillRackWhenStayOnlyThreeTilesOnTheStack() {
 
 		int NUMBER_OF_TILE_IN_THE_RACK = 3;
 
@@ -62,51 +109,57 @@ public class TestForPlayer {
 		stack.add(new Tile(Symbol.RIAS, Color.RED));
 		stack.add(new Tile(Symbol.AKENO, Color.BLUE));
 		stack.add(new Tile(Symbol.KONEKO, Color.YELLOW));
-		ArrayList<Tile> rack = rackCreation();
 		Player player = new Player("Player", stack, 0);
 
 		player.fillRack();
-		int size = player.sizeRack();
 
-		assertThat(size).isEqualTo(NUMBER_OF_TILE_IN_THE_RACK);
+		assertThat(player.sizeRack()).isEqualTo(NUMBER_OF_TILE_IN_THE_RACK);
+		assertThat(stack).isEmpty();
 
 	}
 
 	@Test
 	void shouldClearRack() {
 		ArrayList<Tile> stack = new ArrayList<>();
-		ArrayList<Tile> rack = new ArrayList<>();
-		rack.add(new Tile(Symbol.RIAS, Color.RED));
-		rack.add(new Tile(Symbol.AKENO, Color.BLUE));
-		rack.add(new Tile(Symbol.KONEKO, Color.YELLOW));
-
 		Player player = new Player("Player", stack, 0);
-
-		assertThat(player.sizeRack()).isEqualTo(3);
-		player.clearRack();
-		assertThat(player.sizeRack()).isZero();
-
-	}
-
-	private ArrayList<Tile> rackCreation() {
-		ArrayList<Tile> rack = new ArrayList<>();
-		rack.add(null);
-		rack.add(null);
-		rack.add(null);
-		rack.add(null);
-		rack.add(null);
-		return rack;
-	}
-
-	private ArrayList<Tile> rackFullCreation() {
+		fillRackWithOnlyThreeTiles(player);
+		int startSize = player.sizeRack();
 		
-		ArrayList<Tile> rack = rackCreation();
-		rack.add(new Tile(Symbol.RIAS, Color.RED));
-		rack.add(new Tile(Symbol.AKENO, Color.BLUE));
-		rack.add(new Tile(Symbol.AKENO, Color.GREEN));
-		rack.add(new Tile(Symbol.ASIA, Color.GREY));
-		rack.add(new Tile(Symbol.XENOVIA, Color.YELLOW));
-		return rack;
+		player.clearRack();
+		
+		assertThat(player.sizeRack()).isZero();
+		assertThat(player.sizeRack()).isNotEqualTo(startSize);
+
 	}
 
+	@Test
+	void shouldOnlyLeftOneTileWhenFillARack() {
+		
+		
+	}
+	
+	@Test
+	void shouldLeftEmptyTheSTackAfterFillRAckIfRackIsEmpty() {
+		
+		
+	}
+	private ArrayList<Tile> createASTackWithFiveTile() {
+		ArrayList<Tile> stack = new ArrayList<>();
+		stack.add(new Tile(Symbol.AKENO, Color.RED));
+		stack.add(new Tile(Symbol.AKENO, Color.GREY));
+		stack.add(new Tile(Symbol.RIAS, Color.YELLOW));
+		stack.add(new Tile(Symbol.XENOVIA, Color.BLUE));
+		stack.add(new Tile(Symbol.ASIA, Color.GREEN));
+		;
+		return stack;
+	}
+
+	private void fillRackWithOnlyThreeTiles(Player player) {
+
+		for (int i = 0; i < 3; i++) {
+			if (player.getRack().get(i) == null) {
+				player.getRack().set(i, new Tile(Symbol.RIAS, Color.RED));
+			}
+		}
+	}
 }
